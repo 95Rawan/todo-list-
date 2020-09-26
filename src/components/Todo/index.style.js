cdd26cdd9fd2850524f889c6a923281519bbb346
Rawan Al-Rajabi <rawanalrajabi2016@gmail.com>
2020-09-26 12:26:06 +0300

    Created the todo component and styled it

---
+++ src/components/Todo/index.style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const TodoItem = styled.div`
+  margin: 5px auto;
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const CheckboxTitleContainer = styled.div`
+  width: 100%;
+`;
+
+export const Checkbox = styled.input`
+
+`;
+
+export const Title = styled.span`
+  margin-left:10px;
+`;
+
+export const DeleteButton = styled.button`
+  width:13%;
+`;
